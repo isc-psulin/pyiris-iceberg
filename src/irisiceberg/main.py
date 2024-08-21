2a0cc7325282b5355f0d1101606d2754fb1e1692
isc-patrick <psulin@intersystems.com>
2024-08-21 16:27:14 -0400

    comments

--- src/irisiceberg/main.py
+++ src/irisiceberg/main.py
@@ -109,8 +109,7 @@ class IcebergIRIS:
         partition_size = self.config.table_chunksize
         clause = self.config.sql_clause
 
-        print(self.iris.metadata)
-        # Load data from IRIS table
+        # This commented out connection is DB-API iris library
         #connection = self.iris.engine.connect()
         connection, _ = self.iris.get_odbc_connection()
         for iris_data in read_sql_to_df(connection, tablename, clause=clause, chunksize=partition_size, metadata=self.iris.metadata):
