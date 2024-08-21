b20a860c0664d10ceb98b8c49c75e59bbb051a93
isc-patrick <psulin@intersystems.com>
2024-08-21 15:31:22 -0400

    feat: Add support for ODBC connections and improve SQL data type mapping

--- src/irisiceberg/utils.py
+++ src/irisiceberg/utils.py
@@ -1,59 +1,55 @@
-
+import math 
 from pydantic import BaseModel
 from pydantic import ConfigDict
 from pydantic_settings import BaseSettings
 
-from typing import Optional, List
-#import iris
-import pyodbc 
+from typing import Iterable, Optional, List
+
+import iris
+
 from sqlalchemy import MetaData, create_engine, Table, Column, Integer, String, Float, inspect
 from pyiceberg.schema import Schema
 from pyiceberg.types import NestedField
 import pytest
 import pandas as pd
+from loguru import logger
 
-config1 = {
-    "servers": [
-        {
-            "name": "test",
-            "dialect": "sqlite",
-            "database": "/Users/psulin/projects/insights/python/insights/tests/data/test.db",
-            "schemas": [],
-        },
-        {
-            "name": "LocalIRIS",
-            "dialect": "iris",
-            "database": "User",
-            "driver": "com.intersystems.jdbc.IRISDriver",
-            "host": "localhost",
-            "password": "sys",
-            "user": "_system",
-            "port": 1972,
-            "schemas": ["FS", "SQLUSER"],
-            "field_exclusions": {"table_name": "FS.SecurityMaster", "fields": ["composite_figi"]}
-        },
-    ],
-    "src_server": "LocalIRIS",
-     "icebergs": [
-        {
-            "name": "Test",
-            "uri": "sqlite:////tmp/iceberg/pyiceberg_catalog.db",
-            "warehouse": "/tmp/iceberg",
-            "type": "sqlite",
-        },
-        {
-            "name": "Azure",
-            "uri": "",
-            "adlfs.connection-string": "DefaultEndpointsProtocol=https;AccountName=testiris;AccountKey=g+wwoh1ohZaPrLN+tb+Ed8RE+T4y5au60aptelCbpbIy48v3G1q1biT0MBOgkMSWY6SJFpssi2o8+ASt0hqftQ==;EndpointSuffix=core.windows.net",
-             "adlfs.account-name": "testiris",
-             "type": "azure",
-            "py-io-impl": "pyiceberg.io.fsspec.FsspecFileIO",
-            "database": "testiris",
-            "location": "abfs://mgb"
-        }
-        ],
-        "target_iceberg": "Test"
-}
+ # Create a dictionary to map SQL types to pandas dtypes
+sql_to_pandas_typemap = {
+        'INTEGER': 'int32',
+        'BIGINT': 'int64',
+        'SMALLINT': 'int32',
+        'FLOAT': 'float64',
+        'REAL': 'float32',
+        'DOUBLE': 'float64',
+        'NUMERIC': 'float64',
+        'DECIMAL': 'float64',
+        'CHAR': 'string',
+        'VARCHAR': 'string',
+        'TEXT': 'string',
+        'DATE': 'datetime64[ns]',
+        'TIMESTAMP': 'datetime64[ns]',
+        'BOOLEAN': 'bool',
+        'TINYINT': 'string'
+    }
+
+class IterableWrapper(Iterable):
+    """ Simple wrapping class for an iterator and a set of attribute
+
+    Args:
+        iterator (Iterable): iterator to wrap
+        attributes (dict): dictionary of attributes to return with each iteration
+    """
+    def __init__(self, iterator: Iterable, attributes: dict = {}) -> None:
+        self.iterator = iterator
+        self.attributes = attributes
+            
+    def __iter__(self):
+        for i in self.iterator:
+            yield (i, self.attributes,)
+            
+    def append(self, obj: object, attributes: dict) -> None:
+        self.iterator.append((obj, attributes,))
 
 # Pydantic models are used to validate configurations before code is executed
 class MyBaseModel(BaseSettings):
@@ -95,6 +91,12 @@ def create_connection_url(server: IRIS_Config):
      # Only adding sections if they have a value in the server instance
      
      # sqlite requires 3 slashes for reference to file db
+     if server.connection_type in ['sqlite', 'db-api']:
+         url = get_generic_connection_url(server)
+         return url
+     
+def get_generic_connection_url(server: IRIS_Config):
+    
      seperator = ":///" if server.dialect == "sqlite" else "://"
      driver_dialect = f"{server.dialect}{seperator}" #if not server.driver else f"{server.dialect}+{server.driver}{seperator}"
      user_pass = f"{server.user}:{server.password}@" if server.user and server.password else ""
@@ -102,6 +104,19 @@ def create_connection_url(server: IRIS_Config):
      database = f"{server.database}"
      
      return driver_dialect+user_pass+host_port+database
+
+def get_odbc_connection(server: IRIS_Config):
+    
+    # Added here to prevent loading if not ever using odbc
+    import pyodbc
+
+    con_str = 'DRIVER={Default};SERVER='+server.host+';PORT='+str(server.port)+';DATABASE='+server.database+';UID='+server.user+';PWD='+ server.password
+
+    cnxn = pyodbc.connect(con_str)
+    cnxn.setdecoding(pyodbc.SQL_CHAR, encoding='latin1')
+    cnxn.setencoding(encoding='utf-8', ctype=pyodbc.SQL_CHAR)
+
+    return cnxn, con_str
 
 def get_from_list(lyst: str, name: str) -> MyBaseModel: 
     for item in lyst:
@@ -154,9 +169,84 @@ def sqlalchemy_to_iceberg_schema(table: Table) -> Schema:
 
     return Schema(*iceberg_fields)
 
-def read_sql_with_dtypes(engine, table_name, clause: str  = '', chunksize: int = 5000):
- 
-    # Parse schema and table name
+# def read_with_dtypes(engine, table_name, sql_statements: list = []):
+    
+#     # Parse schema and table name
+#     if '.' in table_name:
+#         schema, table = table_name.split('.', 1)
+#     else:
+#         schema, table = None, table_name
+    
+#     # Get table metadata
+#     inspector = inspect(engine)
+#     columns = inspector.get_columns(table, schema=schema)
+    
+#     # Create a dictionary to map SQL types to pandas dtypes
+#     dtype_map = {
+#         'INTEGER': 'int32',
+#         'BIGINT': 'int64',
+#         'SMALLINT': 'int32',
+#         'FLOAT': 'float64',
+#         'REAL': 'float32',
+#         'DOUBLE': 'float64',
+#         'NUMERIC': 'float64',
+#         'DECIMAL': 'float64',
+#         'CHAR': 'string',
+#         'VARCHAR': 'string',
+#         'TEXT': 'string',
+#         'DATE': 'datetime64[ns]',
+#         'TIMESTAMP': 'datetime64[ns]',
+#         'BOOLEAN': 'bool',
+#         'TINYINT': 'string'
+#     }
+    
+#     # Create a dictionary of column names and their corresponding pandas dtypes
+#     dtypes = {col['name']: dtype_map.get(str(col['type']).split('(')[0].upper(), 'object') 
+#               for col in columns}
+    
+#     # Construct the full table name for the query
+#     full_table_name = f"{schema+'.' if schema else ''}{table}"
+    
+#     # Read the SQL table into a DataFrame with specified dtypes
+#     # TODO - MOve chunkSize to config
+#     query = f"SELECT * FROM {full_table_name} WHERE {clause}"
+#     for df in pd.read_sql(query, engine, dtype=dtypes, chunksize=chunksize):
+
+#         # Convert date and timestamp columns
+#         for col in columns:
+#             if str(col['type']).upper().startswith(('DATE', 'TIMESTAMP')):
+#                 df[col['name']] = pd.to_datetime(df[col['name']])
+        
+#         yield df
+
+def load_data_type_map(tablename, engine):
+    
+    if '.' in tablename:
+        schema, table = tablename.split('.', 1)
+    else:
+        schema, table = None, tablename
+
+    inspector = inspect(engine)
+    columns = inspector.get_columns(table, schema=schema)
+    
+    # Create a dictionary of column names and their corresponding pandas dtypes
+    dtypes = {col['name']: sql_to_pandas_typemap.get(str(col['type']).split('(')[0].upper(), 'object') 
+              for col in columns}
+    
+    return columns, dtypes
+    
+def read_and_write(engine, table_name, sql: str, dtypes:dict, columns, chunksize):
+    
+    pandas_df = pd.read_sql(sql, engine, dtype=dtypes, chunksize=chunksize)
+
+    # Convert date and timestamp columns
+    for col in columns:
+        if str(col['type']).upper().startswith(('DATE', 'TIMESTAMP')):
+            pandas_df[col['name']] = pd.to_datetime(pandas_df[col['name']])
+    
+
+def read_sql_to_df(engine, table_name, clause: str  = '', chunksize: int = 5000, metadata: MetaData = None):
+    
     if '.' in table_name:
         schema, table = table_name.split('.', 1)
     else:
@@ -165,28 +255,13 @@ def read_sql_with_dtypes(engine, table_name, clause: str  = '', chunksize: int =
     # Get table metadata
     inspector = inspect(engine)
     columns = inspector.get_columns(table, schema=schema)
-    
-    # Create a dictionary to map SQL types to pandas dtypes
-    dtype_map = {
-        'INTEGER': 'int32',
-        'BIGINT': 'int64',
-        'SMALLINT': 'int32',
-        'FLOAT': 'float64',
-        'REAL': 'float32',
-        'DOUBLE': 'float64',
-        'NUMERIC': 'float64',
-        'DECIMAL': 'float64',
-        'CHAR': 'string',
-        'VARCHAR': 'string',
-        'TEXT': 'string',
-        'DATE': 'datetime64[ns]',
-        'TIMESTAMP': 'datetime64[ns]',
-        'BOOLEAN': 'bool',
-        'TINYINT': 'string'
-    }
-    
+    cols2 = metadata.tables.get(table_name).columns
+
+    print(columns)
+    print(dict(cols2))
+
     # Create a dictionary of column names and their corresponding pandas dtypes
-    dtypes = {col['name']: dtype_map.get(str(col['type']).split('(')[0].upper(), 'object') 
+    dtypes = {col['name']: sql_to_pandas_typemap.get(str(col['type']).split('(')[0].upper(), 'object') 
               for col in columns}
     
     # Construct the full table name for the query
@@ -194,7 +269,9 @@ def read_sql_with_dtypes(engine, table_name, clause: str  = '', chunksize: int =
     
     # Read the SQL table into a DataFrame with specified dtypes
     # TODO - MOve chunkSize to config
-    query = f"SELECT * FROM {full_table_name} {clause}"
+    where = f"WHERE {clause}" if clause else ''
+    query = f"SELECT * FROM {table_name} {where}"
+    logger.debug(f"Query: {query}")
     for df in pd.read_sql(query, engine, dtype=dtypes, chunksize=chunksize):
 
         # Convert date and timestamp columns
@@ -203,3 +280,82 @@ def read_sql_with_dtypes(engine, table_name, clause: str  = '', chunksize: int =
                 df[col['name']] = pd.to_datetime(df[col['name']])
         
         yield df
+
+def split_sql(tablename, min_id, max_id, partition_size, row_count, clause):
+        """ Generate SQL SELECT statements of equal partitions of records function
+        """
+        part_size = gap_fill_partition(min_id, max_id, partition_size, row_count)
+        logger.info(f"New Part size {part_size}")
+        sql_partitions = generate_select_queries(min_id=min_id, max_id=max_id, 
+                                                partition_size=part_size,
+                                                tablename=tablename, clause=clause) # -> base.IterableWrapper
+
+        logger.info(f"Generated {len(sql_partitions.iterator)} SQL queries")
+        return sql_partitions
+
+def gap_fill_partition(min_id, max_id, partition_size, row_count):
+    """ Adjust the partition size to accomodate for sparsity in the parittion key values"""
+    fullrange = max_id - min_id
+    sparsity = fullrange - row_count
+    if sparsity < 1000:
+        return partition_size
+    multiple = math.ceil(fullrange/row_count)
+    new_part = partition_size * multiple
+    logger.debug(f"New partition size {new_part}")
+    return new_part
+
+def generate_select_queries(min_id: int, max_id: int, partition_size:int, tablename : str,
+                            fields:list = [], clause:str = ""):
+    """ A function that takes a min id, max id, partition size, table name, list of fields and list of clauses 
+    and generates a list of SQL queries selecting all records from a table, where each query selects one parition
+    
+    Args:
+        min_id (int): _description_
+        max_id (int): _description_
+        partition_size (int): _description_
+        table_name (str): _description_
+        fields (list): _description_
+        clauses (list): _description_
+    """
+    #queries = []
+    logger.info(f"generate_select_queries: min_id = {min_id}, max_id = {max_id}")
+    queries_obj = IterableWrapper([])
+    
+    # Set the intitial min and max ids
+    query_max_id = partition_size
+    query_min_id = 0
+    i=0
+    
+    # When the query min id is > than the max_id, then all partitions are complete
+    while query_min_id < max_id:  
+        query = "SELECT "
+        if not fields:
+            query += " * "
+        else:
+            fields = []
+            for field in fields:
+                fields.append(field)
+                
+            query += ", ".join(fields)
+        
+        
+        query += " FROM " + tablename + " WHERE id >= " + str(query_min_id) + \
+                 " AND id < " + str(query_max_id)
+        
+        if clause:
+            query = " AND " + clause
+        
+        queries_obj.append(query, {"table": tablename, "min_id": query_min_id, "max_id": query_max_id})
+        
+        # Update counters for next ierations 
+        query_min_id = (min_id + i) * partition_size
+        query_max_id = (min_id + i + 1) * partition_size
+        i += 1
+   
+    return queries_obj
+
+def downcast_timestamps(df):
+        # Convert all datetime64[ns] columns to datetime64[us]
+        for column in df.select_dtypes(include=['datetime64[ns]']).columns:
+            df[column] = df[column].astype('datetime64[us]')
+        return df
