6f1a457e60f2da1214ce09051059c591013c637c
isc-patrick <psulin@intersystems.com>
2024-09-20 16:18:09 -0400

    add data and scripts

---
+++ scripts/generate_configs.py
@@ -0,0 +1,76 @@
+import os
+import json 
+import sys 
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+base = {
+    "job_type": "list_tables",
+    "table_chunksize": 100,
+    "sql_clause": "",
+    "source_table_name": "DevData",
+    "target_table_name": "DevStats.DevData",
+    "partition_field": "ID",
+    "servers": [
+        {
+            "name": "LocalTesting",
+            "dialect": "sqlite",
+            "database": "/tmp/iceberg/test.db",
+            "warehouse": "/tmp/iceberg",
+            "connection_type": "sqlite",
+            "schemas": [],
+        },
+        {
+            "name": "LocalIRIS",
+            "dialect": "iris",
+            "database": "User",
+            "driver": "com.intersystems.jdbc.IRISDriver",
+            "host": "localhost",
+            "password": "sys",
+            "user": "_system",
+            "port": 5551,
+            "schemas": ["SQLUSER",],
+            "connection_type": "db-api",
+        }
+    ],
+    "icebergs": [
+        {
+            "name": "LocalTesting",
+            "uri": "sqlite:////tmp/iceberg/pyiceberg_catalog.db",
+            "warehouse": "/tmp/iceberg",
+            "type": "sqlite",
+        },
+        {
+            "name": "IRISCatalogLocalWarehouse",
+            "uri": "iris://_SYSTEM:sys@localhost:5551/USER",
+            "warehouse": "/tmp/iceberg",
+            "type": "sqlite",
+        },
+        {
+            "name": "Azure",
+            "uri": "iris://_SYSTEM:sys@localhost:5551/USER",
+            "adls.connection-string": os.environ.get("adls.CONNECTION_STRING"),
+            "adls.account-name": "",
+            "location": "abfs://??",
+        },
+    ] 
+}
+
+local_testing_config = {
+    "src_server": "LocalTesting",
+    "target_iceberg": "LocalTesting",
+}
+
+local_testing_config.update(base)
+
+if __name__ == "__main__":
+
+    current_dir = os.getcwd()
+    config_path = os.path.join(current_dir, 'local_testing_config.json')
+     
+    # Generates a json config for any dictionary defined that ends with _config
+    json.dump(local_testing_config, open(config_path, "w"), indent=2)
+    with open('.env', "w") as fo:
+        fo.writelines(f'IRISICE_CONFIG_PATH="{config_path}"')
